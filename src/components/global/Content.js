522f295b5337559e4e9a23e78dcac2aa91472574
Edson Maciel <eddz-maciel@hotmail.com>
2018-05-16 17:07:47 -0500

    handle click button event example for a counter

--- src/components/global/Content.js
+++ src/components/global/Content.js
@@ -3,11 +3,51 @@ import logo from './images/logo.svg';
 import './css/Content.css';
 
 class Content extends Component {
+  //for read our props
+  constructor(){
+    //to have access to this, we use constructor(props) and super(props);
+    super();
+    this.state={
+      count:0
+    };
+    this.handleCountClick=this.handleCountClick.bind(this);
+  }
+
+  componentDidMount(){
+    this.setState({
+      count:1
+    });
+  }
+
+  handleCountClick(e){
+    console.log(e);
+    if(e.target.id=='add'){
+      this.setState({
+        count: this.state.count+1
+      });
+    } else if(e.target.id=='less' && this.state.count>0){
+      this.setState({
+        count:  this.state.count-1
+      });
+    } else{
+      this.setState({
+        count:0
+      });
+    }
+  }
+
   render() {
+    console.log('render ejecutado');
     return (
       <div className="Content">
-        <h1>Soy el contenido</h1>
-        <p>bla bla bla bla ...</p> 
+        <h2>Counter: {this.state.count}</h2>
+        <p>
+          <button id="add" onClick={this.handleCountClick}>+</button>
+          <button id="less" onClick={this.handleCountClick}>-</button>
+          <button id="rest" onClick={this.handleCountClick}>Reset</button>
+        </p>
+
+
       </div>
     );
   }
--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -12,7 +12,7 @@ class Footer extends Component {
     const{copyright='&copy; React 2018'}=this.props;
     return (
       <div className="Footer">
-        <p dangerouslySetInnerHTML={{__html:copyright}}/>
+        <p dangerouslySetInnerHTML= {{__html:copyright}} />
       </div>
       //we use this dangerouslySetInnerHTML for spetial chars html
     );
