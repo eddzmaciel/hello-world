14ccc5e8a55d863f0b9ca1d5bd7f81f63c806f43
Edson Maciel <eddz-maciel@hotmail.com>
2018-05-16 14:13:45 -0500

    adding items

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
+//  Dependencies
 import React, { Component } from 'react';
-//Components
+
+//  Components
 import Header from './global/Header';
 import Content from './global/Content';
 import Footer from './global/Footer';
+
+// Data
+import Items from '../data/Menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header title="EddzMaciel" items={Items} />
         <Content />
         <Footer />
       </div>
--- src/components/global/Header.js
+++ src/components/global/Header.js
@@ -1,14 +1,29 @@
+// Dependencies
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// Assets
 import logo from './images/logo.svg';
 import './css/Header.css';
 
 class Header extends Component {
+  static propTypes={
+    title:  PropTypes.string.isRequired,
+    items:  PropTypes.array.isRequired
+  };
   render() {
+    console.log(this.props);
+    const { title,items } = this.props;
+    /*const title=this.props.title;
+    const items=this.props.items;*/
     return (
       <div className="Header">
         <div className="Logo">
             <img src={logo} alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>>
+            <h1>{title}</h1>
+            <ul className="Menu">
+              {items && items.map((item,key)=>  <li key={key}>{item.title}</li>)}
+            </ul>
         </div>
       </div>
     );
