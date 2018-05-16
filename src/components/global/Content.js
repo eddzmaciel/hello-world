c8d055c63efd9b463fad4bc72ed82ddafc52a87f
Edson Maciel <eddz-maciel@hotmail.com>
2018-05-16 12:38:31 -0500

    starting

---
+++ src/components/global/Content.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './images/logo.svg';
+import './css/Content.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="Content">
+        <h1>Soy el contenido</h1>
+        <p>bla bla bla bla ...</p> 
+      </div>
+    );
+  }
+}
+
+export default Content;
